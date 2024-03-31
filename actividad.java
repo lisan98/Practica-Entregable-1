7eede641f4f9b3e759e9338f0ea710f3d3d7591b
lisan98 <lisandro0@hotmail.com>
2024-03-31 13:47:22 -0300

    sin tests

---
+++ actividad.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+public class actividad {
+
+    public class alumno{
+        String nombre;
+        materia[] materiasAprobadas = new materia[40];
+    }
+
+    public class materia{
+        String nombre;
+        materia[] correlativas = new materia[40];
+
+        boolean chequearCorrelativas(alumno alumno){
+            boolean estado = false;
+            for( materia correlativa : this.correlativas){
+                for(materia aprobada : alumno.materiasAprobadas ){
+                    if(correlativa.nombre.equals(aprobada.nombre)){
+                        estado = true;
+                        break;
+                    }
+                estado = false;
+                }
+            if(!estado){break;}
+            }
+            return estado;
+        }
+    }
+
+    public class inscripcion{
+        alumno estudiante;
+        materia[] materiasInscripta= new materia[40];
+
+        boolean aprobada(alumno alumno,materia[] materias){
+
+            for(materia materia:materiasInscripta){
+                if(!materia.chequearCorrelativas(this.estudiante)) {
+                    return false;
+                }
+            }
+        return true;
+        }
+    }
+}
